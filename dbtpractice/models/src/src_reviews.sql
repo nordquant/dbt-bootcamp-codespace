3dcff06e36eef5a5b2796e82bb8eed41745a09aa
pradeepkappa <53978964+pradeepkappa@users.noreply.github.com>
2024-05-27 10:25:12 +0000

    Main model commit

---
+++ dbtpractice/models/src/src_reviews.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='table') }}
+with raw_reviews as
+(
+
+    select *from airbnb.raw.raw_reviews
+)
+
+select listing_id,
+date as review_date,
+reviewer_name,
+comments as reviewer_texts,
+sentiment as review_sentiment
+from raw_reviews
