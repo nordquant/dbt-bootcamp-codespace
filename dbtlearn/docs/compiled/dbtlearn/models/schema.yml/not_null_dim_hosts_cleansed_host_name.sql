e374b2e3d5dc29e0b010fc88b32f646de0f57656
Nithish Reddy <43609207+kittureddy05@users.noreply.github.com>
2024-07-27 10:05:52 +0000

    updated_docs

---
+++ dbtlearn/docs/compiled/dbtlearn/models/schema.yml/not_null_dim_hosts_cleansed_host_name.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select host_name
+from AIRBNB.DEV.dim_hosts_cleansed
+where host_name is null
+
+
--- dbtlearn/docs/run/dbtlearn/models/fct/fct_review.sql
+++ dbtlearn/docs/run/dbtlearn/models/fct/fct_review.sql
@@ -1,41 +1,11 @@
-
+-- back compat for old kwarg name
   
+  begin;
     
 
-        create or replace transient table AIRBNB.DEV.fct_review
-         as
+        insert into AIRBNB.DEV.fct_review ("REVIEW_ID", "LISTING_ID", "REVIEW_DATE", "REVIEWER_NAME", "REVIEW_TEXT", "REVIEW_SENTIMENT")
         (
-WITH  __dbt__cte__src_reviews as (
-WITH raw_reviews AS (
- SELECT
- *
- FROM
- AIRBNB.raw.raw_reviews
-)
-SELECT
- listing_id,
- date AS review_date,
- reviewer_name,
- comments AS review_text,
- sentiment AS review_sentiment
-FROM
- raw_reviews
-),src_reviews AS (
- SELECT * FROM __dbt__cte__src_reviews
-)
-SELECT  md5(cast(coalesce(cast(listing_id as 
-    varchar
-), '') || '-' || coalesce(cast(review_date as 
-    varchar
-), '') || '-' || coalesce(cast(reviewer_name as 
-    varchar
-), '') || '-' || coalesce(cast(review_text as 
-    varchar
-), '') as 
-    varchar
-)) as review_id,* FROM src_reviews
-WHERE review_text is not null
-
+            select "REVIEW_ID", "LISTING_ID", "REVIEW_DATE", "REVIEWER_NAME", "REVIEW_TEXT", "REVIEW_SENTIMENT"
+            from AIRBNB.DEV.fct_review__dbt_tmp
         );
-      
-  
+    commit;
