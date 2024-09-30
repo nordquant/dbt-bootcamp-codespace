d184fa845a6facb35b2e0023c8edd7113eab76d3
Zoltan C. Toth <zoltan@zoltanctoth.com>
2024-09-30 13:51:16 +0000

    cleanup

---
+++ .devcontainer/poststart.sh
@@ -0,0 +1,1 @@
+RUN ln -s ${HOME}/profiles.yml ${CODESPACE_VSCODE_FOLDER}/profiles.yml
--- scripts/install-dependencies.sh
+++
@@ -1,3 +0,0 @@
-pip3 install --user -r requirements.txt
-mkdir -p ~/.dbt/
-echo "Hi! Start by executing 'dbt init' to create your first dbt project."
