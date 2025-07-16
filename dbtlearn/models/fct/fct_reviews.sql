be740c8f4b1bd79661b0f4b1d5e0a470fbf35045
AbhishekBheekha <105326031+AbhishekBheekha@users.noreply.github.com>
2025-07-16 23:01:55 +0000

    section13

--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -7,9 +7,12 @@
 WITH src_reviews AS (
   SELECT * FROM {{ ref('src_reviews') }}
 )
-SELECT * FROM src_reviews
+SELECT 
+  {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }}
+    AS review_id,
+  * 
+  FROM src_reviews
 WHERE review_text is not null
-
 {% if is_incremental() %}
   AND review_date > (select max(review_date) from {{ this }})
 {% endif %}
