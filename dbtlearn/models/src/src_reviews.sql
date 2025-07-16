c4642272363f6148c1a88e4037ccba6289dd649d
AbhishekBheekha <105326031+AbhishekBheekha@users.noreply.github.com>
2025-07-16 10:30:20 +0000

    Section 10 prpgress

---
+++ dbtlearn/models/src/src_reviews.sql
@@ -0,0 +1,17 @@
+WITH raw_reviews AS (
+    SELECT
+        *
+    FROM
+        AIRBNB.RAW.RAW_REVIEWS
+)
+SELECT
+    listing_id,
+    date AS review_date,
+    reviewer_name,
+    comments as review_text,
+    sentiment as review_sentiment
+
+FROM
+    raw_reviews
+
+
