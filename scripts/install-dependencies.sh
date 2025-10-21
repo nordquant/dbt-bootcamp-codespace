beccbbcf98c5b72c62bd2f11592cab58969c0031
Zoltan C. Toth <zoltan@zoltanctoth.com>
2025-10-21 18:03:30 +0200

    Update message in install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,3 +1,2 @@
 pip3 install --user -r requirements.txt
-mkdir -p ~/.dbt/
-echo "Hi! Start by executing 'dbt init' to create your first dbt project."
+echo "Hi! Start by executing 'dbt init ...' per the course instructions to create your first dbt project."
